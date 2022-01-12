ac7375d9ec736de2cc1a5caab5a19e19e282539e
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-12 17:21:23 +0200

    Add project files.

---
+++ task_1/task_1.cpp
@@ -0,0 +1,18 @@
+﻿/*
+1. Зациклить программу «Обмен валют», используя цикл с
+постусловием.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, ""); // работает только с потоком вывода
+
+	
+
+	return 0;
+}
---
+++ task_3/task_3.cpp
@@ -0,0 +1,18 @@
+﻿/*
+3. В цикле с клавиатуры вводятся 10 целых чисел.
+Вычислить процент чётных чисел и процент нечётных.
+*/
+
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_ALL, ""); // работает только с потоком вывода
+
+
+
+	return 0;
+}
