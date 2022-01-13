aaa191c801f57874d517f78494f7d1468fafb11c
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-01-13 20:33:44 +0200

    +3 и 2 задание, осталось 1 доделать

--- task_2/task_2.cpp
+++ task_2/task_2.cpp
@@ -13,7 +13,27 @@ int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
 
+	int number{ 0 }, negative_numbers{ 0 }, zeros{ 0 }, positive_numbers{ 0 };
+	double amount_of_numbers{ 0 };
+	cout << "Введите 10 целых чисел: " << endl;
 
+	do
+	{
+		cin >> number;
+
+		amount_of_numbers++; // подсчитываем кол-во введенных чисел
+
+		if (number < 0) // считаем отрицательные числа
+			negative_numbers++;
+		else if (number == 0)
+			zeros++; // считаем нули
+		else if (number > 0)
+			positive_numbers++; // считаем положительные числа
+	} while (amount_of_numbers < 10);
+
+	cout << (double)amount_of_numbers / 100 * negative_numbers * 100 << "% отрицательных чисел." << endl;
+	cout << (double)amount_of_numbers / 100 * zeros * 100 << "% нулей." << endl;
+	cout << (double)amount_of_numbers / 100 * positive_numbers * 100 << "% положительных чисел." << endl;
 
 	return 0;
 }
--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -12,12 +12,24 @@ int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
 
-	int number_1{ 0 }, number_1{ 0 }, number_1{ 0 },рпмрпарп
-		number_1{ 0 }, number_1{ 0 }, number_1{ 0 },
-		number_1{ 0 }, number_1{ 0 }, number_1{ 0 },
-		number_1{ 0 };
+	int number{ 0 }, amount_of_numbers{ 0 }, even_number{ 0 }, odd_number{ 0 };
 
-	cout<<""
+	cout << "Введите 10 целых чисел: " << endl;
+
+	do
+	{
+		cin >> number;
+
+		amount_of_numbers++; //подсчитываем кол-во введенных чисел
+
+		if (number % 2 == 0) //считаем четные числа
+			even_number++;
+		else
+			odd_number++; //считаем не четные числа
+	} while (amount_of_numbers < 10);
+
+	cout << (double)amount_of_numbers / 100 * even_number * 100 << "% четных чисел." << endl;
+	cout << (double)amount_of_numbers / 100 * odd_number * 100 << "% не четных чисел." << endl;
 
 	return 0;
 }
